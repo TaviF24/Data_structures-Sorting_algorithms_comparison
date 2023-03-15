09d4790703c6dec043f650c6009e587b9b88b53b
TaviF24 <118764142+TaviF24@users.noreply.github.com>
2023-03-16 00:27:44 +0200

    main

--- sortari.py
+++ sortari.py
@@ -2,11 +2,12 @@ def counting_sort(lista,nr_max):
     v_frecventa=[0]*(nr_max+1)
     for element in lista:
         v_frecventa[element]+=1
-    j=0
-    for x in range(0,nr_max+1):
-        for i in range(0,v_frecventa[x]):
-            lista[j]=x
-            j+=1
+    for i in range(1,len(v_frecventa)):
+        v_frecventa[i]+=v_frecventa[i-1]
+    copie=lista.copy()
+    for i in range(len(lista)-1,-1,-1):
+        v_frecventa[copie[i]]-=1
+        lista[v_frecventa[copie[i]]]=copie[i]
 
 # lista=[2,1,100,45,24,24,2000,46,45,6,3,89,0,35,1000]
 # print(lista)
